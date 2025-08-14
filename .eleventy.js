697b0f425ce00bec0385f45f986acd956159e82f
ThiloChalas <thilo.chalas@epfl.ch>
2025-08-14 12:32:27 +0200

    Glideandseek embed filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
   // Tell Eleventy to copy the entire 'src/assets' folder as-is to '_site/assets'
   eleventyConfig.addPassthroughCopy("src/assets");
+  // Tell Eleventy to copy the 'src/assets/glideandseek' folder as-is to '_site/assets/glideandseek'
+  eleventyConfig.addPassthroughCopy("src/assets/glideandseek");
+  
 
   return {
     dir: {
