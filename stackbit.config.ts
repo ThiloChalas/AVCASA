663fe6ff1de4c7d38f5a2f26c48e6fbb5996f88f
ThiloChalas <thilo.chalas@epfl.ch>
2025-08-16 11:48:02 +0200

    update stackbit

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,26 @@
-import { defineStackbitConfig } from '@stackbit/types';
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "eleventy",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["src"],
+      models: [
+        {
+          name: "page",
+          label: "Site Page",
+          type: "page",
+          filePath: "src/{slug}.njk",
+          urlPath: "/{slug}",
+          fields: [
+            { name: "title", type: "string", label: "Page Title", required: true },
+            { name: "layout", type: "string", label: "Layout" },
+            { name: "bannerHeading", type: "string", label: "Banner Heading" },
+            { name: "bannerSubheading", type: "string", label: "Banner Subheading" }
+          ]
+        }
+      ]
+    })
+  ]
+});
