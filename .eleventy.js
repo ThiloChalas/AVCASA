f7bebb8214002aaaf3986a404d345546c930861e
ThiloChalas <thilo.chalas@epfl.ch>
2026-09-03 15:23:27 +0200

    Modify page description metadata + add sitemap for google search

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+const sitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(sitemap, {
+    sitemap: {
+      hostname: "https://avcasa.fr"
+    }
+  });
+
   // Tell Eleventy to copy the entire 'src/assets' folder as-is to '_site/assets'
   eleventyConfig.addPassthroughCopy("src/assets");
   // Tell Eleventy to copy the 'src/assets/glideandseek' folder as-is to '_site/assets/glideandseek'
